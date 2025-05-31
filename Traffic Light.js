6078bfc9822b5d554164abecfc919279cdca389f
kokkulatrilok03 <kokkulatrilok005@gmail.com>
2025-05-31 16:17:07 +0530

    Initial commit

---
+++ Traffic Light.js
@@ -0,0 +1,44 @@
+function stopButton() {
+    let buttonstop = document.getElementById("stopLight");
+    buttonstop.style.backgroundColor = "red";
+    let colorButton = document.getElementById("stopButton");
+    colorButton.style.backgroundColor = "red";
+    let readyInStop = document.getElementById("readyLight");
+    readyInStop.style.backgroundColor = "#4b5069";
+    let readyButtonInstop = document.getElementById("readyButton");
+    readyButtonInstop.style.backgroundColor = "#1f1d41";
+    let goInstop = document.getElementById("goLight");
+    goInstop.style.backgroundColor = "#4b5069";
+    let goButtonInstop = document.getElementById("goButton");
+    goButtonInstop.style.backgroundColor = "#1f1d41";
+}
+
+function readyButton() {
+    let buttonstop = document.getElementById("readyLight");
+    buttonstop.style.backgroundColor = "#f7c948";
+    let colorButton = document.getElementById("readyButton");
+    colorButton.style.backgroundColor = "#f7c948";
+    let stopInready = document.getElementById("stopLight");
+    stopInready.style.backgroundColor = "#4b5069";
+    let stopButtonInReady = document.getElementById("stopButton");
+    stopButtonInReady.style.backgroundColor = "#1f1d41";
+    let goinReady = document.getElementById("goLight");
+    goinReady.style.backgroundColor = "#4b5069";
+    let goButtonInReady = document.getElementById("goButton");
+    goButtonInReady.style.backgroundColor = "#1f1d41";
+}
+
+function goButton() {
+    let buttonstop = document.getElementById("goLight");
+    buttonstop.style.backgroundColor = "#199473";
+    let colorButton = document.getElementById("goButton");
+    colorButton.style.backgroundColor = "#199473";
+    let stopingo = document.getElementById("stopLight");
+    stopingo.style.backgroundColor = "#4b5069";
+    let stopButtonIngo = document.getElementById("stopButton");
+    stopButtonIngo.style.backgroundColor = "#1f1d41";
+    let redayIngo = document.getElementById("readyLight");
+    redayIngo.style.backgroundColor = "#4b5069";
+    let ReadyButtonIngo = document.getElementById("readyButton");
+    ReadyButtonIngo.style.backgroundColor = "#1f1d41";
+}
